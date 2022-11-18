b97b300a25769a7b787c65159bedf2be626ca875
Jiang <jiangyue0380@gmail.com>
2022-11-18 15:29:54 +1100

    updated CollectionSerializer to show minifigs_num field

--- backend/collection/serializer.py
+++ backend/collection/serializer.py
@@ -6,5 +6,5 @@ class ColletionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Collection
         fields = ['user_id', 'set_num', 'set_name', 'release_year', 'theme',
-                  'num_parts', 'set_img_url', 'purchase_date', 'build_completion_date']
+                  'num_parts', 'minifigs_num', 'set_img_url', 'purchase_date', 'build_completion_date']
         depth = 1
