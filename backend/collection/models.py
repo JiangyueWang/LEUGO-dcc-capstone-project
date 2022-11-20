23cb864906cff548006f34c2b0f209e0ef654621
Jiang <jiangyue0380@gmail.com>
2022-11-20 21:53:23 +1100

    created Wishlist model for collection app + migrated to lego_collection_db database

--- backend/collection/models.py
+++ backend/collection/models.py
@@ -15,3 +15,11 @@ class Collection(models.Model):
     purchase_date = models.DateField()
     # build_completion_date can be null when adding a set into the database
     build_completion_date = models.DateField(blank=True, null=True)
+
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    set_num = models.CharField(max_length=100)
+    set_name = models.CharField(max_length=100)
+    release_year = models.IntegerField()
+    theme = models.CharField(max_length=100)
